dae3d3f65076b63bd542c4fe9fa8d197a5def16c
santen <st3rax@gmail.com>
2015-03-18 03:46:32 +0000

    add model users

--- src/UserBundle/Controller/MainController.php
+++ src/UserBundle/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends Controller
 {
@@ -12,18 +13,31 @@ class MainController extends Controller
      * @Route("/register")
      * @Template()
      */
-    public function registerAction()
-    {
+    public function registerAction(){
         return $this->render('default/reg.html.twig');
     }
 
     /**
-     * @Route("/profile/{id}")
+     * @Route("/adduser", name = "adduser")
+     */
+    public function addUserAction(Request $request){
+        $user = array();
+        array_push($user, $request->request->get("mail"));
+        array_push($user, $request->request->get("passwd1"));
+        array_push($user, $request->request->get("passwd2"));
+
+        
+
+        return $this->redirectToRoute('profile', array('id' => $user[0]));
+    }
+
+    /**
+     * @Route("/profile/{id}", name = "profile")
      * @Template()
      */
     public function profileAction($id)
     {	
     	$user = "st3rax";
-        return $this->render("default/profile.html.twig", array('user' => $user));
+        return $this->render("default/profile.html.twig", array('user' => $id));
     }
 }
---
+++ src/UserBundle/Entity/Users.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Users
+ */
+class Users
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nickname;
+
+    /**
+     * @var string
+     */
+    private $mail;
+
+    /**
+     * @var string
+     */
+    private $passwd;
+
+    /**
+     * @var integer
+     */
+    private $posts;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nickname
+     *
+     * @param string $nickname
+     * @return Users
+     */
+    public function setNickname($nickname)
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    /**
+     * Get nickname
+     *
+     * @return string 
+     */
+    public function getNickname()
+    {
+        return $this->nickname;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     * @return Users
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string 
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set passwd
+     *
+     * @param string $passwd
+     * @return Users
+     */
+    public function setPasswd($passwd)
+    {
+        $this->passwd = $passwd;
+
+        return $this;
+    }
+
+    /**
+     * Get passwd
+     *
+     * @return string 
+     */
+    public function getPasswd()
+    {
+        return $this->passwd;
+    }
+
+    /**
+     * Set posts
+     *
+     * @param integer $posts
+     * @return Users
+     */
+    public function setPosts($posts)
+    {
+        $this->posts = $posts;
+
+        return $this;
+    }
+
+    /**
+     * Get posts
+     *
+     * @return integer 
+     */
+    public function getPosts()
+    {
+        return $this->posts;
+    }
+}
